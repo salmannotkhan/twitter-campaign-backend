6418bb6d77f7c33c2a9ad9b78d477f5ff8374d5e
Salman Shaikh <tony903212@gmail.com>
2022-01-07 15:25:15 +0530

    Added helper functions for twitter data

---
+++ src/helpers/twitter.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+const wait = (ms) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+};
+
+export const getUsername = (users) => {
+    const LINK_REGEX = /(?<=twitter.com\/).*(?=\?)/;
+    const ALT_REGEX = /(?<=twitter.com\/).*/;
+    const STATUS_REGEX = /\/status.*/;
+
+    return users.map((user) => {
+        var username;
+        if (LINK_REGEX.test(user)) {
+            username = LINK_REGEX.exec(user)[0];
+        } else if (ALT_REGEX.test(user)) {
+            username = ALT_REGEX.exec(user)[0];
+        }
+        if (!username) {
+            return;
+        }
+        username = username.replace("@", "");
+        username = username.replace(STATUS_REGEX, "");
+        return username;
+    });
+};
+
+export const getFollowers = async (usernames) => {
+    var usersData = [];
+    const headers = {
+        Authorization: `Bearer ${process.env.TOKEN}`,
+    };
+    const url = "https://api.twitter.com/2/users/by";
+    for (let index = 0; index < usernames.length; index += 100) {
+        const params = {
+            usernames: usernames.slice(index, index + 100).join(),
+            "user.fields": "public_metrics",
+        };
+        const response = await axios.get(url, {
+            headers,
+            params,
+        });
+        usersData = usersData.concat(response.data.data);
+        await wait(1000);
+    }
+    return usersData;
+};
